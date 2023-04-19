aaf049dbaad464a553ae28e8f3367a3f8447e66f
Liuk23 <ldvcoding@gmail.com>
2023-04-19 20:58:57 +0200

    added help command

--- Commands.cs
+++ Commands.cs
@@ -1,17 +1,47 @@
 ﻿using System;
 using Discord.Commands;
 using System.Threading.Tasks;
+using Discord;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Liuk_Music_CS
 {
 	public class Commands : ModuleBase<SocketCommandContext>
 	{
-		Program program = new Program();
-
 		[Command("ping")]
+		[Summary("It makes sure the bot is running properly.")]
 		public async Task Ping()
 		{
-			await ReplyAsync($"Latency: {program.Client.Latency} ms.");
+			await ReplyAsync($"Latency: {Program.Client.Latency} ms.");
+		}
+
+
+		[Command("help")]
+		[Summary("It gives a simple explanation about each command.")]
+		public async Task Help()
+		{
+			var embed = new EmbedBuilder()
+			{
+				Title = "HELP",
+				// Description = "It gives a simple explanation about each command.",
+				Timestamp = DateTime.Now
+			};
+			foreach (var command in Program.CommandsService.Commands)
+			{
+				embed.AddField($"__{command.Name}__", command.Summary, true);
+			}
+
+			await ReplyAsync(embed: embed.Build());
+		}
+
+
+		[Command("play")]
+		[Summary("It plays the specified song in the user's voice channel.")]
+		public async Task Play()
+		{
+
 		}
 	}
 }
--- Program.cs
+++ Program.cs
@@ -13,27 +13,15 @@ using System.Windows.Input;
 
 namespace Liuk_Music_CS
 {
-	public class Program
+	public static class Program
 	{
-		private static DiscordSocketClient _client;
-		private static CommandService _commands;
-		private IServiceProvider _services;
+		public static DiscordSocketClient Client { get; set; }
+		public static CommandService CommandsService { get; set; }
+		public static IServiceProvider Services { get; set; }
 
-		static void Main(string[] args) => new Program().RunBotAsync().GetAwaiter().GetResult();
+		static void Main(string[] args) => RunBotAsync().GetAwaiter().GetResult();
 
-		public DiscordSocketClient Client
-		{
-			set
-			{
-				return;
-			}
-			get
-			{
-				return _client;
-			}
-		}
-
-		public async Task RunBotAsync()
+		public static async Task RunBotAsync()
 		{
 			var _config = new DiscordSocketConfig
 			{
@@ -41,35 +29,35 @@ namespace Liuk_Music_CS
 				MessageCacheSize = 1000,
 				GatewayIntents = GatewayIntents.All
 			};
-			_client = new DiscordSocketClient(_config);
-			_commands = new CommandService();
-			_services = new ServiceCollection()
-				.AddSingleton(_client)
-				.AddSingleton(_commands)
+			Client = new DiscordSocketClient(_config);
+			CommandsService = new CommandService();
+			Services = new ServiceCollection()
+				.AddSingleton(Client)
+				.AddSingleton(CommandsService)
 				.BuildServiceProvider();
 
 			await InstallCommandsAsync();
-			await _client.LoginAsync(Discord.TokenType.Bot, secrets.Default.token);
-			await _client.StartAsync();
+			await Client.LoginAsync(Discord.TokenType.Bot, secrets.Default.token);
+			await Client.StartAsync();
 
-			_client.Log += Client_Log;
-			_client.MessageUpdated += MessageUpdated;
-			_client.Ready += () => { return Task.CompletedTask; };
+			Client.Log += Client_Log;
+			Client.MessageUpdated += MessageUpdated;
+			Client.Ready += () => { return Task.CompletedTask; };
 
 			await Task.Delay(-1);
 		}
 
-		private Task Client_Log(LogMessage arg)
+		private static Task Client_Log(LogMessage arg)
 		{
 			Console.WriteLine(arg);
 			return Task.CompletedTask;
 		}
 
 		//Register Commands Async
-		public async Task InstallCommandsAsync()
+		public static async Task InstallCommandsAsync()
 		{
 			// Hook the MessageReceived event into our command handler
-			_client.MessageReceived += HandleCommandAsync;
+			Client.MessageReceived += HandleCommandAsync;
 
 			// Here we discover all of the command modules in the entry 
 			// assembly and load them. Starting from Discord.NET 2.0, a
@@ -79,11 +67,12 @@ namespace Liuk_Music_CS
 			//
 			// If you do not use Dependency Injection, pass null.
 			// See Dependency Injection guide for more information.
-			await _commands.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), _services);
+			await CommandsService.AddModulesAsync(assembly: Assembly.GetEntryAssembly(), Services);
+			Console.WriteLine(string.Join("\n", CommandsService.Commands));
 		}
 
 		// Read Input and get Output ( RECEIVE AND DO SOMETHING )
-		private async Task HandleCommandAsync(SocketMessage messageParam)
+		private static async Task HandleCommandAsync(SocketMessage messageParam)
 		{
 			// Don't process the command if it was a system message
 			var message = messageParam as SocketUserMessage;
@@ -94,15 +83,15 @@ namespace Liuk_Music_CS
 			int argPos = 0;
 
 			// Determine if the message is a command based on the prefix and make sure no bots trigger commands
-			if (!(message.HasCharPrefix(secrets.Default.prefix, ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos)) || message.Author.IsBot)
+			if (!(message.HasCharPrefix(secrets.Default.prefix, ref argPos) || message.HasMentionPrefix(Client.CurrentUser, ref argPos)) || message.Author.IsBot)
 				return;
 
 			// Create a WebSocket-based command context based on the message
-			var context = new SocketCommandContext(_client, message);
+			var context = new SocketCommandContext(Client, message);
 
 			// Execute the command with the command context we just
 			// created, along with the service provider for precondition checks.
-			var result = await _commands.ExecuteAsync(
+			var result = await CommandsService.ExecuteAsync(
 				context: context,
 				argPos: argPos,
 				services: null);
@@ -111,7 +100,7 @@ namespace Liuk_Music_CS
 				Console.WriteLine(result.ErrorReason);
 		}
 
-		private async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
+		private static async Task MessageUpdated(Cacheable<IMessage, ulong> before, SocketMessage after, ISocketMessageChannel channel)
 		{
 			// If the message was not in the cache, downloading it will result in getting a copy of `after`.
 			var message = await before.GetOrDownloadAsync();
